cf9eea116f276c9e691c7daa01dc5447e3f14b2e
Cameron Glass <cameron.glass@lightelligence.ai>
2022-04-13 16:59:31 -0400

    Removed stale files

--- test/regenerate_golden.sh
+++ test/regenerate_golden.sh
@@ -2,7 +2,6 @@
 bazel build //test/...
 cp bazel-bin/test/golden_inputs/*_rypkg.svh  test/golden_outputs/
 cp bazel-bin/test/golden_inputs/*_pkg_*.h  test/golden_outputs/
-# cp bazel-bin/test/golden_inputs/*_mem.svh    test/golden_outputs/
 cp bazel-bin/test/golden_inputs/*_rypkg.html test/golden_outputs/
 cp bazel-bin/test/golden_inputs/*_intf.html  test/golden_outputs/
 cp bazel-bin/test/golden_inputs/*_pkg_*.rdl  test/golden_outputs/
